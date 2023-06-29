70c0cade50b6ba75c7b45237fdcfebbb6c258f14
Fernando Serra <fernando.serra.r@gmail.com>
2023-06-29 16:12:01 -0400

    Actualizacion general

---
+++ app/src/main/java/com/codemobile/demo_link/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.codemobile.demo_link
+
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val action: String? = intent?.action
+        //val data: Uri? = intent?.data
+        val data: String? = intent?.data?.getQueryParameter("p")
+
+
+        Toast.makeText(this, "data enviada: $data", Toast.LENGTH_SHORT).show()
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/codemobile/demo_link/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.codemobile.demo_link.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
